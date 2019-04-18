e5e600cfda3dbdd93d6aed688cabc6811e84c85f
cdcardle <cdcardle@gmail.com>
2019-04-17 20:44:13 -0700

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class UserInput extends Component {
+  state = {
+    username: '',
+    hometown: ''
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.username !== '') {
+      this.props.store.dispatch({ type: 'ADD_USER', user: this.state});
+      this.setState({username: '', hometown: ''});
+    }
+  }
+
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={this.handleSubmit}>
+          <label for="username">Username: </label>
+          <input type="text" name="username" onChange={this.handleChange} value={this.state.username} />
+          <br/>
+          <label for="hometown">Hometown: </label>
+          <input type="text" name="hometown" onChange={this.handleChange} value={this.state.hometown} />
+          <br/>
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
